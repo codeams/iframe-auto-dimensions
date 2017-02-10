1afe6f3187686ff2eed756b8a5e358207d5cd597
Erick Montañez <codeams@gmail.com>
2017-02-10 04:25:59 -0600

    Separating plugin from example

--- autoHeight.js
+++ autoHeight.js
@@ -56,15 +56,4 @@ $(function() {
 
   };
 
-
-  var $iframe = $( 'iframe' );
-  $iframe.autoHeight();
-
-
-  // Test height change:
-  setTimeout(function() {
-    for ( var i = 0; i < 2000; i++ )
-      $( 'body', $iframe.contents() ).append( '<h1>Added content.</h1>' );
-  }, 3000);
-
 });
